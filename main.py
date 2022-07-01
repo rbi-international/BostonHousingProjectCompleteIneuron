6b98995065315d2aa105ba1ad902964e0250966c
Rohit Bharti <rohit.bharti8211@gmail.com>
2022-07-01 17:32:24 +0530

    adding first file structure

---
+++ main.py
@@ -0,0 +1,19 @@
+from distutils.log import debug
+from flask import Flask
+from housing.exception import HousingException
+from housing.logger import logging
+import sys
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "CI CD Pipeline has been established"
+
+if __name__=="__main__":
+    app.run(debug = True)
